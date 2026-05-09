cb943b285e1f6ff56d3a90862747a95e76fb5974
Danstiv <50794055+Danstiv@users.noreply.github.com>
2026-05-10 05:35:13 +0700

    Update addon url

--- buildVars.py
+++ buildVars.py
@@ -33,7 +33,7 @@ addon_info = {
 	# Author(s)
 	"addon_author": "Danstiv <danstiv@yandex.ru>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/Danstiv/URLPlayer",
+	"addon_url": "https://github.com/addonWorkshop/URLPlayer",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
